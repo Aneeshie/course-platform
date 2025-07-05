94b9f04bc36001f18b96d2f357cb090594fe2a20
Aneeshie <aneeshdas556@gmail.com>
2025-07-05 15:37:28 +0530

    feat: course display

--- convex/schema.ts
+++ convex/schema.ts
@@ -6,5 +6,12 @@ export default defineSchema({
     name: v.string(),
     email: v.string(),
     clerkId: v.string(),
-  }).index("by_clerkId", ["clerkId"]),
+  }).index("by_clerkId", ["clerkId"]),  
+
+  courses: defineTable({
+    title: v.string(),
+    description: v.string(),
+    imageUrl: v.string(),
+    price: v.number()
+  })
 });
